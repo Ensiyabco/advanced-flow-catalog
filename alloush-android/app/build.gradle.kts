fb7e63366204dfe239eaeae474a35e41c0f22bce
Ensiyabco <aloshm1@gmail.com>
2026-09-19 19:12:36 +0300

    Add Alloush Android V1: alloush-android/app/build.gradle.kts

---
+++ alloush-android/app/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+android {
+    namespace = "com.ensiyabco.alloush"
+    compileSdk = 35
+    defaultConfig {
+        applicationId = "com.ensiyabco.alloush"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "0.1.0"
+    }
+}
